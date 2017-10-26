0f4c290a28aa37f89c52588acc67e112bd7331da
Elias Kotlyar <elias.kotlyar@gmail.com>
2017-10-26 19:18:52 +0200

    Unpacker V1

---
+++ firmware/unpacker.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import click
+
+
+@click.command()
+@click.argument('file', default="")
+def cli(file):
+    dic = [
+        ("kernel", 0x200000),
+        ("root", 0x350000),
+        ("driver", 0xa0000),
+        ("appfs", 0x4a0000),
+        ("backupk", 0x200000),
+        ("backupd", 0xa0000),
+        ("backupa", 0x200000),
+        ("config", 0x40000),
+        ("para", 0x40000),
+        ("flag", 0x10000)
+    ]
+    file = "demo_5.5.1.194.bin"
+    fullflash = open(file, 'rb')
+    fullflash.seek(128)
+    for name, size in dic:
+        filename = "flash/" + name + ".zip"
+        buffer = fullflash.read(size)
+        f = open(filename, "wb")
+        f.write(buffer)
+
+
+if __name__ == '__main__':
+    cli()
