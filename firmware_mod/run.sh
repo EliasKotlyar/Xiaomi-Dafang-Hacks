387aa061c986b43d7d15f94262580ae45a278b14
Elias Kotlyar <elias.kotlyar@gmail.com>
2017-11-30 23:35:19 +0100

    Bftpd

--- firmware_mod/run.sh
+++ firmware_mod/run.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+CONFIGPATH=/system/sdcard/config
+SAMPLE_CONFIGPATH=/system/sdcard/sample_config
+## Copy Configs:
+if [  ! -f $CONFIGPATH/wpa_supplicant.conf ]; then
+	cp $SAMPLE_CONFIGPATH/wpa_supplicant.conf $CONFIGPATH/wpa_supplicant.conf
+fi
+if [  ! -f $CONFIGPATH/bftpd.conf ]; then
+	cp $SAMPLE_CONFIGPATH/bftpd.conf $CONFIGPATH/bftpd.conf
+fi
+
+
 ## Start Wifi:
 wpa_supplicant -B -i wlan0 -c /system/sdcard/wpa_supplicant.conf -P /var/run/wpa_supplicant.pid
 udhcpc -i wlan0 -p /var/run/udhcpc.pid -b
@@ -8,7 +19,7 @@ insmod /driver/sensor_jxf22.ko data_interface=2 pwdn_gpio=-1 reset_gpio=18 senso
 
 ## Start FTP & SSH
 /system/sdcard/bin/dropbearmulti dropbear -R
-/system/dafang/bin/bftpd -d
+/system/sdcard/bin/bftpd -d
 
 
 
