4340b198c65070c123f1178c28ef33731836cec6
Elias Kotlyar <elias.kotlyar@gmail.com>
2017-12-05 16:12:27 +0100

    Motor and run.sh

--- firmware_mod/run.sh
+++ firmware_mod/run.sh
@@ -10,9 +10,30 @@ udhcpc -i wlan0 -p /var/run/udhcpc.pid -b
 
 ## Start Audio:
 insmod /driver/audio.ko
+
+## Start GPIO:
+setgpio () {
+GPIOPIN=$1
+echo $GPIOPIN > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio$GPIOPIN/direction
+echo 0 > /sys/class/gpio/gpio$GPIOPIN/active_low
+echo 1 > /sys/class/gpio/gpio$GPIOPIN/value
+}
+# IR-LED
+setgpio 49
+# Yellow-LED
+setgpio 38
+# Blue-LED
+setgpio 39
+
+# Startup Motor:
+insmod /system/sdcard/driver/sample_motor
+
+
+
 ## Start Sensor:
-insmod /driver/tx-isp.ko isp_clk=100000000
-insmod /driver/sensor_jxf22.ko data_interface=2 pwdn_gpio=-1 reset_gpio=18 sensor_gpio_func=0
+insmod /system/sdcard/driver/tx-isp.ko isp_clk=100000000
+insmod /system/sdcard/driver/sensor_jxf22.ko data_interface=2 pwdn_gpio=-1 reset_gpio=18 sensor_gpio_func=0
 
 ## Start FTP & SSH
 /system/sdcard/bin/dropbearmulti dropbear -R
