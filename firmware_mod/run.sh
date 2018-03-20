5f5e2804c9a97f2ba06eae7bf6cb3e1fa48b2878
Ryan Jarvis <ryan@shopboxretail.com>
2018-03-19 18:35:39 -0700

    Cleanup script using shellcheck.  Whitespace.  Refactored a fragile for loop and removed a "dead cat"

--- firmware_mod/run.sh
+++ firmware_mod/run.sh
@@ -7,24 +7,25 @@ echo "Starting up CFW"
 hostname -F $CONFIGPATH/hostname.conf
 
 ## Get real Mac address from config file:
-MAC=`cat /params/config/.product_config | grep MAC | cut -c16-27 | sed 's/\(..\)/\1:/g;s/:$//'`
+MAC=$(grep MAC < /params/config/.product_config | cut -c16-27 | sed 's/\(..\)/\1:/g;s/:$//')
 
 ## Start Wifi:
 insmod /driver/8189es.ko rtw_initmac="$MAC"
 wpa_supplicant -B -i wlan0 -c $CONFIGPATH/wpa_supplicant.conf -P /var/run/wpa_supplicant.pid
-udhcpc -i wlan0 -p /var/run/udhcpc.pid -b -x hostname:`hostname`
+udhcpc -i wlan0 -p /var/run/udhcpc.pid -b -x hostname:"$(hostname)"
 
 ## Start Audio:
 insmod /system/sdcard/driver/audio.ko
 
 ## Start GPIO:
 setgpio () {
-GPIOPIN=$1
-echo $GPIOPIN > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio$GPIOPIN/direction
-echo 0 > /sys/class/gpio/gpio$GPIOPIN/active_low
-echo 1 > /sys/class/gpio/gpio$GPIOPIN/value
+  GPIOPIN=$1
+  echo "$GPIOPIN" > /sys/class/gpio/export
+  echo out > /sys/class/gpio/gpio"$GPIOPIN"/direction
+  echo 0 > /sys/class/gpio/gpio"$GPIOPIN"/active_low
+  echo 1 > /sys/class/gpio/gpio"$GPIOPIN"/value
 }
+
 # IR-LED
 setgpio 49
 echo 1 > /sys/class/gpio/gpio49/active_low
@@ -40,8 +41,6 @@ setgpio 26
 
 # Startup Motor:
 insmod /system/sdcard/driver/sample_motor.ko
-
-
 
 ## Start Sensor:
 insmod /system/sdcard/driver/tx-isp.ko isp_clk=100000000
@@ -59,26 +58,25 @@ insmod /system/sdcard/driver/sensor_jxf22.ko data_interface=2 pwdn_gpio=-1 reset
 
 ## Get OSD-Information
 if [ -f /system/sdcard/config/osd ]; then
-source /system/sdcard/config/osd  2>/dev/null
+  source /system/sdcard/config/osd  2>/dev/null
 fi
 
 ## Autostart
- for i in `ls /system/sdcard/config/autostart/`; do /system/sdcard/config/autostart/$i; done
+for i in /system/sdcard/config/autostart/*; do
+  $i
+done
 
 #Start
-
 /system/sdcard/bin/busybox nohup /system/sdcard/bin/v4l2rtspserver-master &>/dev/null &
-
 if [ -f /system/sdcard/config/osd ]; then
-	source /system/sdcard/config/osd
-	/system/sdcard/bin/setconf -k o -v "${OSD}"
-        /system/sdcard/bin/setconf -k c -v ${COLOR}
-        /system/sdcard/bin/setconf -k s -v ${SIZE}
-        /system/sdcard/bin/setconf -k x -v ${POSY}
-        /system/sdcard/bin/setconf -k w -v ${FIXEDW}
-	/system/sdcard/bin/setconf -k p -v ${SPACE}
+  source /system/sdcard/config/osd
+  /system/sdcard/bin/setconf -k o -v "${OSD}"
+  /system/sdcard/bin/setconf -k c -v "${COLOR}"
+  /system/sdcard/bin/setconf -k s -v "${SIZE}"
+  /system/sdcard/bin/setconf -k x -v "${POSY}"
+  /system/sdcard/bin/setconf -k w -v "${FIXEDW}"
+  /system/sdcard/bin/setconf -k p -v "${SPACE}"
 fi;
-
 
 echo "Startup finished!"
 
