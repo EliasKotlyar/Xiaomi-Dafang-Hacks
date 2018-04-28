f8150f6d7cbd880e5c4c8e0895ccd5a68d311ea7
phineasthecat <hellomunkey333@gmail.com>
2018-04-27 22:30:13 -0400

    merge rcfile.sh and rcfile_wyzecam.sh
    does a simple 'does this file exist' if check instead of two separate files

--- hacks/firmware/create_hacked_firmware_wyzecam_v2.sh
+++ hacks/firmware/create_hacked_firmware_wyzecam_v2.sh
@@ -4,7 +4,7 @@ TMPDIR=./rootfs
 OUTFILE=./rootfs.bin
 rm -r $TMPDIR $OUTFILE
 unsquashfs -d $TMPDIR $FIRMWARE_ROOT/rootfs.bin
-cp ./rcfile_wyzecam.sh $TMPDIR/etc/init.d/rcS
+cp ./rcfile.sh $TMPDIR/etc/init.d/rcS
 mksquashfs $TMPDIR $OUTFILE -b 1048576 -comp xz -Xdict-size 100%
 ./packer.py $FIRMWARE_ROOT/kernel.bin $OUTFILE $FIRMWARE_ROOT/driver.bin $FIRMWARE_ROOT/appfs.bin firmware_hacked.bin
 rm -r $TMPDIR $OUTFILE
