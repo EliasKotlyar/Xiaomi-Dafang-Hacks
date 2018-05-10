c1e14659989444ead4cc096ae48810d3791777b5
Patrick Fruh <Kaeltis@users.noreply.github.com>
2018-05-10 20:16:52 +0200

    Fix greenish video color #344 and version display (#346)
    * Fix greenish video color #344
    
    * use POSIX -R instead of -r
    
    * also copy /etc/os-release
    
    otherwise the version display is broken

--- firmware_mod/run.sh
+++ firmware_mod/run.sh
@@ -25,9 +25,9 @@ if [ ! -d /system/sdcard/root ]; then
 fi
 if [ ! -d /system/sdcard/etc ]; then
   mkdir /system/sdcard/etc
-  cp -f /etc/TZ /etc/protocols /etc/fstab /etc/inittab /etc/hosts \
+  cp -fRL /etc/TZ /etc/protocols /etc/fstab /etc/inittab /etc/hosts \
     /etc/passwd /etc/shadow /etc/group /etc/resolv.conf /etc/hostname \
-    /etc/profile /system/sdcard/etc
+    /etc/profile /etc/os-release /etc/sensor /system/sdcard/etc
   sed -i s#/:#/root:# /system/sdcard/etc/passwd
   echo "Created etc directory on sdcard" >> $LOGPATH
 fi
