a82ba59cdf6b7c55868b8c3c38a1c81c2fea85bb
neroxps <neroxps@gmail.com>
2018-06-03 23:07:11 +0800

    Fix the coordinate display of the new motor command.

--- firmware_mod/scripts/common_functions.sh
+++ firmware_mod/scripts/common_functions.sh
@@ -366,7 +366,7 @@ auto_night_mode(){
 # Update axis
 update_axis(){
   source /system/sdcard/config/osd.conf > /dev/null 2>&1
-  AXIS=`/system/sdcard/bin/motor -d s | tail +5 | awk '{printf ("%s ",$0)}' |  awk '{print "X="$2,"Y="$4}'`
+  AXIS=`/system/sdcard/bin/motor -d s | sed '3d' | awk '{printf ("%s ",$0)}' | awk '{print "X="$2,"Y="$4}'`
   if [ $DISPLAY_AXIS ]; then
     OSD=$(echo ${OSD} | sed -r "s/X=.*$/${AXIS}/")
   fi
