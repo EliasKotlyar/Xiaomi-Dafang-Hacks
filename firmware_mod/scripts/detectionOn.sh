a4363f944ba38274fdbd40a0f04b4b6600afef35
Åskar Andersson <askar.andersson@gmail.com>
2018-06-08 18:40:28 +0200

    Update detectionOn.sh

--- firmware_mod/scripts/detectionOn.sh
+++ firmware_mod/scripts/detectionOn.sh
@@ -11,7 +11,6 @@ fi
 
 # Save a snapshot
 if [ "$save_snapshot" = true ] ; then
-	save_dir=/system/sdcard/motion/stills
 	filename=$(date +%d-%m-%Y_%H.%M.%S).jpg
 	if [ ! -d "$save_dir" ]; then
 		mkdir -p $save_dir
