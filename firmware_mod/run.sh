55e6bc027439bcb8ff343bcfc7841a2c6c4af5be
drcobra <panos@panos.me.uk>
2018-06-11 13:57:10 +0100

    renamed lighttpd.conf & hostname.conf to .dist and init from it (#448)
    * renamed lighttpd.conf to .dist and init from it
    
    * renamed hostname.conf to .dist and init from it
    
    * lighttpd.conf path using $CONFIGPATH

--- firmware_mod/run.sh
+++ firmware_mod/run.sh
@@ -63,6 +63,9 @@ fi
 wpa_supplicant_status="$(wpa_supplicant -B -i wlan0 -c $CONFIGPATH/wpa_supplicant.conf -P /var/run/wpa_supplicant.pid)"
 echo "wpa_supplicant: $wpa_supplicant_status" >> $LOGPATH
 
+if [ ! -f $CONFIGPATH/hostname.conf ]; then
+  cp $CONFIGPATH/hostname.conf.dist $CONFIGPATH/hostname.conf
+fi
 hostname -F $CONFIGPATH/hostname.conf
 udhcpc_status=$(udhcpc -i wlan0 -p /var/run/udhcpc.pid -b -x hostname:"$(hostname)")
 echo "udhcpc: $udhcpc_status" >> $LOGPATH
@@ -129,6 +132,9 @@ if [ ! -f $CONFIGPATH/lighttpd.pem ]; then
 fi
 
 ## Start Webserver:
+if [ ! -f $CONFIGPATH/lighttpd.conf ]; then
+  cp $CONFIGPATH/lighttpd.conf.dist $CONFIGPATH/lighttpd.conf
+fi
 lighttpd_status=$(/system/sdcard/bin/lighttpd -f /system/sdcard/config/lighttpd.conf)
 echo "lighttpd: $lighttpd_status" >> $LOGPATH
 
