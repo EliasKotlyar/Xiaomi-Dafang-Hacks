d272843ef6d4af039b3e2aa45bff62b543b766e2
Ryan Jarvis <ryan@shopboxretail.com>
2018-07-14 22:51:52 -0700

    Add a couple missing {} to getThemeChoice()

--- firmware_mod/www/scripts/index.html.js
+++ firmware_mod/www/scripts/index.html.js
@@ -173,10 +173,12 @@ function getCookie(name) {
     var ca = document.cookie.split(';');
     for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0) === ' ')
+        while (c.charAt(0) === ' ') {
             c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0)
-            return decodeURIComponent(c.substring(nameEQ.length, c.length));
+            if (c.indexOf(nameEQ) === 0) {
+                return decodeURIComponent(c.substring(nameEQ.length, c.length));
+            }
+        }
     }
     return null;
 }
