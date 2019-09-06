25db7d729f1837e21f460eb3abcd1a8f719b486d
jmtatsch <jmtatsch@users.noreply.github.com>
2019-09-06 10:55:03 +0200

    Update detectionOn.sh
    Fix #957 telegram video not working

--- firmware_mod/scripts/detectionOn.sh
+++ firmware_mod/scripts/detectionOn.sh
@@ -191,7 +191,9 @@ if [ "$send_telegram" = true ]; then
 		/system/sdcard/bin/telegram p "$snapshot_tempfile"
 	elif [ "$telegram_alert_type" = "video" ] ; then
 		debug_msg "Send telegram video"
-		/system/sdcard/bin/telegram v "$video_tempfile"
+		/system/sdcard/bin/avconv -i "$video_tempfile"  "${video_tempfile}-lo.mp4"
+		/system/sdcard/bin/telegram v "${video_tempfile}-lo.mp4"
+		rm "${video_tempfile}-lo.mp4"
 	fi
 	) &
 fi
