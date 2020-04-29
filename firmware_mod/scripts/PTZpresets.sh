4e54a7ab92483f190ab5746bb7a7f19706cc9e91
Solomon S <slm4996@users.noreply.github.com>
2020-04-29 13:37:06 -0400

    Complete rewrite of PTZpresets.sh
    Rewrite PTZpresets.sh to have better PID handling and full posix compatibility.

--- firmware_mod/scripts/PTZpresets.sh
+++ firmware_mod/scripts/PTZpresets.sh
@@ -1,124 +1,170 @@
 #!/bin/sh
 
-################################################
-# Created by Nero                              #
-# neroxps@gmail.com | 2018-5-28 | v0.0.6 Beta  #
-# Extended to support presets by slm4996       #
-# slm4996+github@gmail.com | 2020-4-28         #
-################################################
+#######################################################
+# Accepts either presets or step coordinates          #
+# Created by slm4996                                  #
+# slm4996+github@gmail.com | 4-29-2020 | v0.0.1 Beta  #
+#######################################################
 
-source /system/sdcard/scripts/common_functions.sh
+set -e
 
+LOG=false
+
+logger() {
+    echo "$1"
+    if $LOG; then
+        echo "$(date '+%Y-%m-%d-%H:%M:%S') $1" >> /system/sdcard/log/ptz.log
+    fi
+}
+
+PIDFILE="/var/run/PTZpresets.pid"
+
+# check pidfile
+if [ -e "$PIDFILE" ] && [ -e "/proc/$(cat $PIDFILE)" ]
+then
+    # A process exists with our saved PID
+    logger "PTZpresets.sh is already running with PID $PID_SAVED; exiting"
+    exit 1
+fi
+
+# write pidfile
+if ! logger $$ >"$PIDFILE"
+then
+    # If we couldn't save the PID to the lockfile...
+    logger "Failed to create PID file for PID $$ in $PIDFILE; exiting"
+    exit 1
+fi
+
+trap 'rm "$PIDFILE"' EXIT
+
+# Include common_functions from Dafang-Hacks
+# shellcheck disable=SC1091
+. /system/sdcard/scripts/common_functions.sh
+
+# Path to motor binary
 MOTOR=/system/sdcard/bin/motor.bin
-pid=$$
-echo $pid > /run/PTZ_$pid.pid
 
+# Path to ptz_presets.conf
 FILEPRESETS=/system/sdcard/config/ptz_presets.conf
 
-# Check for presets file load defaults if not present
+# Check for custom presets file, load examples if not present
 if [ ! -f ${FILEPRESETS} ]; then
-  cp /system/sdcard/config/ptz_presets.conf.dist /system/sdcard/config/ptz_presets.conf
+    cp /system/sdcard/config/ptz_presets.conf.dist /system/sdcard/config/ptz_presets.conf
 fi
 
-# set log
-if [[ -z $3 ]]; then
-    LOG=false
-else
-    LOG=true
-fi
-
-logger(){
-   if $LOG; then
-        echo "$(date '+%Y-%m-%d-%H:%M:%S') $1" >> /system/sdcard/log/ptz.log
-   fi
+# Print usage
+print_usage() {
+    logger "Usage:"
+    logger "Presets: PTZpresets.sh preset [preset_name]"
+    logger "Example: PTZpresets.sh preset home"
+    logger "Presets are defined in /system/sdcard/config/ptz_presets.conf.dist."
+    logger ""
+    logger "Steps:   PTZpresets.sh [X axis steps] [Y axis steps]"
+    logger "Example: PTZpresets.sh 1400 350"
 }
 
-# exit
-exit_shell(){
-    rm -f /run/PTZ_$pid.pid
-    exit $1
+# Calculate relative steps for axis
+calculate_relative_steps() {
+    case $1 in
+        x|X)
+            current_x_axis=$($MOTOR -d s | grep "$1" | awk '{print $2}')
+            relative_x_steps=$(echo $(($2 - current_x_axis)) | sed 's/-//')
+        ;;
+        y|Y)
+            current_y_axis=$($MOTOR -d s | grep "$1" | awk '{print $2}')
+            relative_y_steps=$(echo $(($2 - current_y_axis)) | sed 's/-//')
+        ;;
+        *)
+            exit 1
+        ;;
+    esac
 }
 
-
-# Get axis status
-get_steps(){
-    val="$($MOTOR -d s | grep $1 | awk '{print $2}')"
-    echo $val
-}
-
-move(){
-	# Differentiate the coordinates and calculate the number
-	# of steps to reach the specified coordinates.
-	if [[ "$1" = "X" ]];then 
-		grep_text="x"
-		opt="r|l"
-		text="Right|Left"
-	else
-		grep_text="y"
-		opt="u|d"
-		text="Up|Down"
-	fi
-	SRC_STEPS=$(get_steps $grep_text)
-	DST_STEPS=$2
-    STEPS=$(awk -v a="$DST_STEPS" -v b="$SRC_STEPS" 'BEGIN{printf ("%d",a-b)}')
-
-    # Motor runs 1.3 time as long as the number of steps.
-    SLEEP_NUM=$(awk -v a="${STEPS//-/}" 'BEGIN{printf ("%f",a*1.3/1000)}')
-
-    # "+" Right or Up, "-" Left or Down.
-    if [[ $STEPS -gt 0 ]]; then
-        $MOTOR -d ${opt%|*} -s "${STEPS//-/}" &>/dev/null
-        logger "$1 axis: DST:$DST_STEPS SRC:$SRC_STEPS ${text%|*} $STEPS"
+# Handle preset arguments
+preset() {
+    target_x_axis=$(jq ".presets.${1}.x" ${FILEPRESETS})
+    target_y_axis=$(jq ".presets.${1}.y" ${FILEPRESETS})
+    if [ "$target_y_axis" = 'null' ]; then
+        logger "Error: Preset \"$1\" is not defined!"
+        exit 1
     else
-        $MOTOR -d ${opt#*|} -s "${STEPS//-/}" &>/dev/null
-        logger "$1 axis DST:$DST_STEPS SRC:$SRC_STEPS ${text#*|} $STEPS"
+        logger "Loading preset $1: X=$target_x_axis, Y=$target_y_axis"
     fi
-
-    # Waiting for the motor to run.
-    sleep ${SLEEP_NUM}
 }
 
-# If the previous instruction did not complete, wait for it to complete before continuing.
-whit_pid=$(cat /run/PTZ* 2>/dev/null | awk -v pid=$pid '$1<pid{print $1}')
-while [[ "$whit_pid" != "" ]] ;do
-	whit_pid=$(cat /run/PTZ* 2>/dev/null | awk -v pid=$pid '$1<pid{print $1}')
-    sleep 1
-done
+# Handle location arguments
+location() {
+    case $2 in
+        [0-9]*)
+            target_x_axis=$1
+            target_y_axis=$2
+            logger "Location defined: X=$target_x_axis, Y=$target_y_axis"
+        ;;
+        *)
+            print_usage
+            exit 1
+        ;;
+    esac
+}
 
 # Main
-# If no arguments are provided then go to preset "home"
-if [ $# -eq 0 ]; then
-  # Get values in saved presets file
-  target_x_axis=`cat ${FILEPRESETS} | jq '.presets.home.x'`
-  target_y_axis=`cat ${FILEPRESETS} | jq '.presets.home.y'`
-elif [ "$1" == 'preset' ]; then
-  target_x_axis=`cat ${FILEPRESETS} | jq ".presets.${2}.x"`
-  target_y_axis=`cat ${FILEPRESETS} | jq ".presets.${2}.y"`
+case "$1" in
+    preset)
+        preset "$2"
+    ;;
+    [0-9]*)
+        location "$1" "$2"
+    ;;
+    *)
+        print_usage
+        exit 1
+    ;;
+esac
+
+calculate_relative_steps "x" "$target_x_axis"
+calculate_relative_steps "y" "$target_y_axis"
+
+if [ "$relative_x_steps" -ne 0 ]; then
+    if [ "$target_x_axis" -lt "$current_x_axis" ]; then
+        dir="l"
+    else
+        dir="r"
+    fi
+
+    CMD="$MOTOR -d $dir -s $relative_x_steps"
+    logger "$CMD"
+    $CMD > /dev/null 2>&1
+
+    # Motor runs 1.3 time as long as the number of steps.
+    SLEEP_NUM=$(awk -v a="$relative_x_steps" 'BEGIN{printf ("%f",a*1.3/1000)}')
+
+    # Wait for motor to run
+    sleep "$SLEEP_NUM"
 else
-  case "$1" in
-    *[!0-9]*|"")
-      logger "Usage: $(basename $0) [axis_X number] [axis_Y number]"
-      exit_shell 2
-      ;;
-    [0-9]*)
-      target_x_axis=$1
-      ;;
-  esac
-  case "$2" in
-    *[!0-9]*|"")
-      logger "Usage: $(basename $0) [axis_X number] [axis_Y number]"
-      exit_shell 2
-      ;;
-    [0-9]*)
-      target_y_axis=$2
-      ;;
-  esac
+    logger "X axis is already at position: $current_x_axis"
 fi
 
-move X $target_x_axis
-move Y $target_y_axis
+if [ "$relative_y_steps" -ne 0 ]; then
+    if [ "$target_y_axis" -lt "$current_y_axis" ]; then
+        dir="d"
+    else
+        dir="u"
+    fi
+
+    CMD="$MOTOR -d $dir -s $relative_y_steps"
+    logger "$CMD"
+    $CMD > /dev/null 2>&1
+
+    # Motor runs 1.3 time as long as the number of steps.
+    SLEEP_NUM=$(awk -v a="$relative_y_steps" 'BEGIN{printf ("%f",a*1.3/1000)}')
+
+    # Wait for motor to run
+    sleep "$SLEEP_NUM"
+else
+    logger "Y axis is already at position: $current_y_axis"
+fi
 
 # Update OSD_AXIS
 update_axis
-logger "Move end motor coordinates:$AXIS"
-exit_shell 0
+
+exit 0
