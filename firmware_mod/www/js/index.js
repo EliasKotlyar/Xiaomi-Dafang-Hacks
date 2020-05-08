f9d94775da5399f83ef20a20bc8bfdd5a6818d94
gysmo38 <frederic@leroux.im>
2020-05-08 14:55:04 +0200

    Control and system config page finish :)

--- firmware_mod/www/js/index.js
+++ firmware_mod/www/js/index.js
@@ -1,3 +1,5 @@
+var version
+
 //Function to get parameters URL
 $.urlParam = function(name){
     var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
@@ -7,15 +9,44 @@ $.urlParam = function(name){
     return decodeURI(results[1]) || 0;
 }
 
+//Function to open tab in pages
+function openTab(evt, tabName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("tab");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" w3-border-theme", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.firstElementChild.className += " w3-border-theme";
+  }
+
+//Function show/hide accordion
+function accordion() {
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+  
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      } 
+    });
+  }
+}
+
 $(document).ready(function () {
    
-    // Set title page
-    $.get("cgi-bin/state.cgi", {cmd: "hostname"}, function(title){document.title = title;document.getElementById("title").innerHTML = title;});
-
+  
     // Set git version to bottom page
     $.get("cgi-bin/state.cgi", {cmd: "version"}, function(version){document.getElementById("version").innerHTML = version;});
-
-
 
     // Display content depend url parameter
     if ( $.urlParam('url') != null )
@@ -24,6 +55,7 @@ $(document).ready(function () {
         var url = "live.html"
 
     $("#content").load(url);
+
 });
 
 
