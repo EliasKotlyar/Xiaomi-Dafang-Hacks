72ee688aa5a3f9ab557bca2a5c4895c917cab7eb
gysmo38 <frederic@leroux.im>
2020-05-25 22:06:18 +0200

    Improve SD Card files

--- firmware_mod/www/js/sdcard.js
+++ firmware_mod/www/js/sdcard.js
@@ -1,3 +1,12 @@
+//Function to delete a file
+function deleteFile(fileName,dir) {
+    var del = confirm("Confirm delete file: "+fileName);
+    if ( del ) { 
+        $.get("cgi-bin/sdcard.cgi", {cmd: "del_file",file: fileName});
+        getFiles(dir);
+    }
+}
+
 //Function to get video and images files from dir
 function getFiles(dir) {
     // Get files from dir
@@ -8,7 +17,14 @@ function getFiles(dir) {
             $('#tab'+dir).append("<tr><td colspan='4'>No files found</td></tr>");
         for (var i = 0; i < config_all.length-1; i++) {
          var config_info = config_all[i].split("#:#");       
-         $('#tab'+dir).append("<tr><td>"+config_info[0]+"</td><td>"+config_info[1]+"</td><td>"+config_info[2]+"</td><td><a href=\""+config_info[3]+"\">Download</a></td></tr>");       
+         $('#tab'+dir).append("<tr> \
+         <td>"+config_info[0]+"</td> \
+         <td>"+config_info[1]+"</td> \
+         <td>"+config_info[2]+"</td> \
+         <td> \
+             <a href=\""+config_info[3]+"\"><i class='fas fa-download'></i></a> \
+              | <span onclick=\"deleteFile('"+config_info[3]+"','"+dir+"')\"><i class='fas fa-trash'></i></span> \
+         </td></tr>");       
         }
 
     });
@@ -19,7 +35,7 @@ function onLoad() {
     //Activate accordion
     accordion();
     //Get configuration
-    getFiles('recording');
+    getFiles('motion');
 }
 
 onLoad();
