bef6c757fdfb0cb77cf4008c7f2d4dfa7f29a629
gysmo38 <frederic@leroux.im>
2020-06-18 12:52:10 +0200

    Bug fix in merge script

--- firmware_mod/scripts/merge_config.sh
+++ firmware_mod/scripts/merge_config.sh
@@ -8,8 +8,8 @@ for CONF in $CONF_LIST; do
     CONF_NEW_VARS_COUNT=0
     if [ -f "${CONF_PATH}/${CONF}" ]; then
         echo "Check ${CONF}"
-        CONF_DIST_VARS=$(grep -o '^[^#]*' "${CONF}.dist" | cut -d= -f1 | sort)
-        CONF_USER_VARS=$(grep -o '^[^#]*' $CONF | cut -d= -f1 | sort)
+        CONF_DIST_VARS=$(grep -o '^[^#]*' "${CONF_PATH}/${CONF}.dist" | cut -d= -f1 | sort)
+        CONF_USER_VARS=$(grep -o '^[^#]*' ${CONF_PATH}/${CONF} | cut -d= -f1 | sort)
         for VAR in $CONF_DIST_VARS; do
             if [ ! -n  "$(echo $CONF_USER_VARS | grep $VAR)" ]; then
                 CONF_NEW_VARS_COUNT=$((CONF_NEW_VARS_COUNT+1))
