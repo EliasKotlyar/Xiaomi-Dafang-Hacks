49720d0c36ac0f236c5b90e8223cdf5acc6cf954
Will Freeman <6641284+TheRealWaldo@users.noreply.github.com>
2021-04-27 14:24:32 -0600

    Update root ca and updater

--- firmware_mod/config/ssl/cacert/update.sh
+++ firmware_mod/config/ssl/cacert/update.sh
@@ -7,6 +7,6 @@
 # We don't mind you downloading the PEM file from us in an automated fashion,
 # but please don't do it more often than once per day. It is only updated once
 # every few months anyway.
-# ABOUT: https://curl.haxx.se/docs/caextract.html
+# ABOUT: https://curl.se/docs/caextract.html
 
-curl --remote-name --time-cond cacert.pem https://curl.haxx.se/ca/cacert.pem
+curl --remote-name --time-cond cacert.pem https://curl.se/ca/cacert.pem
